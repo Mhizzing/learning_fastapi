f13d6d91c5a1e509be090b2ebdea5300e8793032
Winston Lam <wertyserty@hotmail.com>
2022-06-08 00:25:34 +1000

    updated main.py

--- main.py
+++ main.py
@@ -55,3 +55,43 @@ async def get_model(model_name: ModelName):
 async def read_file(file_path: str):
     return {"file_path": file_path}
     
+# Query parameters
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10): # We can access these query parameters using key-value pairs after ?, separated by & in the URL
+    return fake_items_db[skip : skip + limit]
+
+# Accessing /items/ would be the same as /items/?skip=0&limit=10
+
+
+# Optional parameters
+from typing import Union
+
+
+@app.get("/things/{thing_id}")
+async def read_thing(thing_id: str, q: Union[str, None] = None, short: bool = False): # q is an optional str query parameter, short is required boolean query parameter.
+    thing = {"thing_id": thing_id}
+    if q:
+        thing.update({"q": q})
+    if not short:
+        thing.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return thing
+
+# Path and query parameters are detected by name, and can be declared at the same time in any order
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
